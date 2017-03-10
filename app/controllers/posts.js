f424aae0479b917865833c5132608d0582c05549
karbz0ne <dev@blaszczyk.tk>
2017-03-10 21:00:16 +0100

    []- Added a new base Route. api/random. It should return a random post from database

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -5,14 +5,30 @@ export default class {
         PostModel.find({})
             .limit(5)
             .exec((err, posts) => {
-            if (err) return res.json({error: err.errors, Response: {message: err.message}});
-            if (!posts) return res.json({error: true, Response: {message: 'Error while fetching posts'}});
+                if (err) return res.json({error: err.errors, Response: {message: err.message}});
+                if (!posts) return res.json({error: true, Response: {message: 'Error while fetching posts'}});
 
-            res.status(200).json(posts);
+                res.status(200).json(posts);
         });
     };
 
     Random = (req, res) => {
+        PostModel
+            .count()
+            .exec((err, count) => {
+                if (err) return res.json({error: err.errors, Response: {message: err.message}});
+                if (!count) return res.json({error: true, Response: {message: 'Error while fetching posts'}});
 
+                const random = Math.floor(Math.random() * count);
+
+                PostModel
+                    .findOne()
+                    .skip(random)
+                    .exec((err, post) => {
+                        if (err) return res.json({error: err.errors, Response: {message: err.message}});
+
+                        res.status(200).json(post);
+                });
+        });
     };
 };
